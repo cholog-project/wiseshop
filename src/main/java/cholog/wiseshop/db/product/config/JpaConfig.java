d5e67afcc2cf9d188799a5583439fbb57fed64eb
gogo1414 <wnstn6945@gmail.com>
2025-01-06 13:51:10 +0900

    feature: JPA Configuration 설정 구현

---
+++ src/main/java/cholog/wiseshop/db/product/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package cholog.wiseshop.db.product.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}

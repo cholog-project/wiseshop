2b62affac51c912e8af226d11f18e15d6871cbd6
gogo1414 <wnstn6945@gmail.com>
2025-01-06 13:35:10 +0900

    feture: h2 설정 및 상품 엔티티 구현

---
+++ src/main/java/cholog/wiseshop/db/product/Product.java
@@ -0,0 +1,42 @@
+package cholog.wiseshop.db.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    public Product(Long id,
+                   String name,
+                   int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}

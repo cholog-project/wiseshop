9cf078495b9ea1ce75dff493addbbb0ca25689ba
gogo1414 <wnstn6945@gmail.com>
2025-01-07 10:28:06 +0900

    refactor: config 폴더 이동

--- src/main/java/cholog/wiseshop/common/config/JpaConfig.java
+++ src/main/java/cholog/wiseshop/common/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package cholog.wiseshop.db.product.config;
+package cholog.wiseshop.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;

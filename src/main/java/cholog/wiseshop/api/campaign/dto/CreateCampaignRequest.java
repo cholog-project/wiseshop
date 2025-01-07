7016e4324790ecf8159b194c8908053a124edcbc
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-01-07 23:21:02 +0900

    feat: 캠페인 등록 API 테스트 구현 및 확인

--- src/main/java/cholog/wiseshop/api/campaign/dto/CreateCampaignRequest.java
+++ src/main/java/cholog/wiseshop/api/campaign/dto/CreateCampaignRequest.java
@@ -1,9 +1,14 @@
 package cholog.wiseshop.api.campaign.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
-public record CreateCampaignRequest(LocalDateTime startDate,
-                                    LocalDateTime endDate,
-                                    int goalQuantity,
-                                    Long productId) {
+public record CreateCampaignRequest(
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        LocalDateTime startDate,
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        LocalDateTime endDate,
+        int goalQuantity,
+        Long productId) {
 }
--- src/main/java/cholog/wiseshop/db/campaign/Campaign.java
+++ src/main/java/cholog/wiseshop/db/campaign/Campaign.java
@@ -1,6 +1,10 @@
 package cholog.wiseshop.db.campaign;
 
 import cholog.wiseshop.db.product.Product;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,8 +24,12 @@ public class Campaign {
     @JoinColumn(name = "PRODUCT_ID")
     private Product product;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime startDate;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime endDate;
 
     private int goalQuantity;

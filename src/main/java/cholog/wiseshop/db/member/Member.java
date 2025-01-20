ed2688bc34ab34b10353ff82d668b5dfbd6b8984
Sumin <new.sumyang@gmail.com>
2025-01-20 14:33:00 +0900

    feat: Member 엔티티 추가

---
+++ src/main/java/cholog/wiseshop/db/member/Member.java
@@ -0,0 +1,26 @@
+package cholog.wiseshop.db.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "member_email", nullable = false)
+    private String email;
+
+    @NotNull
+    @Column(name = "password", nullable = false)
+    private String password;
+}

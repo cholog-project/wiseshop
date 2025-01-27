f0d9a5e38938ce3d74355473ff73f3cddde971a4
gogo1414 <wnstn6945@gmail.com>
2025-01-27 15:06:40 +0900

    feature: 주문 목록 조회 테스트 구현 - 주문 목록 조회 시 발생하는 외래키 Unique 속성 제한으로 기존 상품, 주문의  1:1 관게를 1:N 관계로 변경

--- src/main/java/cholog/wiseshop/db/order/Order.java
+++ src/main/java/cholog/wiseshop/db/order/Order.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -19,8 +20,8 @@ public class Order extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "PRODUCT_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PRODUCT_ID", unique = false)
     private Product product;
 
     private int count;

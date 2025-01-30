8f6ea6248dd7d3b5b2acb56a006e5042a91f181a
Sumin <new.sumyang@gmail.com>
2025-01-30 16:53:11 +0900

    refactor: 회원 기능 예외 커스텀 적용

--- src/main/java/cholog/wiseshop/api/member/service/MemberService.java
+++ src/main/java/cholog/wiseshop/api/member/service/MemberService.java
@@ -4,6 +4,8 @@ import cholog.wiseshop.api.member.dto.request.SignInRequest;
 import cholog.wiseshop.api.member.dto.request.SignUpRequest;
 import cholog.wiseshop.db.member.Member;
 import cholog.wiseshop.db.member.MemberRepository;
+import cholog.wiseshop.exception.WiseShopErrorCode;
+import cholog.wiseshop.exception.WiseShopException;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,7 @@ public class MemberService {
 
     public void signUpMember(SignUpRequest signUpRequest) {
         if (memberRepository.findByEmail(signUpRequest.email()).isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+            throw new WiseShopException(WiseShopErrorCode.ALREADY_EXIST_MEMBER);
         }
         String encodePassword = passwordEncoder.encode(signUpRequest.password());
         Member member = new Member(signUpRequest.email(), signUpRequest.name(), encodePassword);
@@ -33,10 +35,10 @@ public class MemberService {
 
     public void signInMember(SignInRequest signInRequest, HttpSession session) {
         Member member = memberRepository.findByEmail(signInRequest.email())
-            .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 아이디 입니다."));
+            .orElseThrow(() -> new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND));
         boolean matches = passwordEncoder.matches(signInRequest.password(), member.getPassword());
         if (!matches) {
-            throw new IllegalArgumentException("올바르지 않은 아이디 입니다.");
+            throw new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND);
         }
         session.setAttribute(SESSION_KEY, member);
     }

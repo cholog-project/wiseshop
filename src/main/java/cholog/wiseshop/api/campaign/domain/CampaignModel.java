f4985c8638ce948416bd21fcb2dc1ad03beb35c8
최준호 <junho5336@gmail.com>
2025-02-04 23:36:18 +0900

    feat: campaign model 구현

--- src/main/java/cholog/wiseshop/api/campaign/domain/CampaignModel.java
+++ src/main/java/cholog/wiseshop/api/campaign/domain/CampaignModel.java
@@ -1,5 +1,49 @@
 package cholog.wiseshop.api.campaign.domain;
 
-public class CampaignModel {
+import cholog.wiseshop.api.member.domain.MemberModel;
+import cholog.wiseshop.api.product.domain.ProductModel;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CampaignModel {
+    private final Long goalQuantity;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+    private final CampaignStatus status;
+    private final MemberModel supplier;
+    private final List<ProductModel> products;
+
+    public CampaignModel(Long goalQuantity, LocalDateTime startDate, LocalDateTime endDate, CampaignStatus status, MemberModel supplier, List<ProductModel> products) {
+        this.goalQuantity = goalQuantity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.supplier = supplier;
+        this.products = products;
+    }
+
+    public Long getGoalQuantity() {
+        return goalQuantity;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public CampaignStatus getStatus() {
+        return status;
+    }
+
+    public MemberModel getSupplier() {
+        return supplier;
+    }
+
+    public List<ProductModel> getProducts() {
+        return products;
+    }
 }
---
+++ src/main/java/cholog/wiseshop/api/campaign/domain/CampaignStatus.java
@@ -0,0 +1,9 @@
+package cholog.wiseshop.api.campaign.domain;
+
+public enum CampaignStatus {
+    WAITING,
+    IN_PROGRESS,
+    FAILED,
+    SUCCESS,
+    ;
+}

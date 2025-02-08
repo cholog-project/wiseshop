8f71d1beb77ecfaad713f7c0f2d7d6ce96cf41ad
Sumin <new.sumyang@gmail.com>
2025-02-08 22:49:18 +0900

    refactor: 캠페인 생성 로직 리팩토링

--- src/main/java/cholog/wiseshop/api/product/dto/request/CreateProductRequest.java
+++ src/main/java/cholog/wiseshop/api/product/dto/request/CreateProductRequest.java
@@ -3,10 +3,12 @@ package cholog.wiseshop.api.product.dto.request;
 import cholog.wiseshop.db.product.Product;
 import cholog.wiseshop.db.stock.Stock;
 
-public record CreateProductRequest(String name,
-                                   String description,
-                                   int price,
-                                   int totalQuantity) {
+public record CreateProductRequest(
+    String name,
+    String description,
+    int price,
+    int totalQuantity
+    ) {
 
     public Product from() {
         return new Product(

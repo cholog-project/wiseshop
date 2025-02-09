22dc6af12fb164ee4b2a77607bd0dbefd587167b
gogo1414 <wnstn6945@gmail.com>
2025-02-09 18:40:46 +0900

    style: 라인 포매팅 적용

--- src/main/java/cholog/wiseshop/api/member/service/MemberService.java
+++ src/main/java/cholog/wiseshop/api/member/service/MemberService.java
@@ -19,45 +19,45 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberService {
 
-	private static final String SESSION_KEY = "member";
-	private final MemberRepository memberRepository;
-	private final CampaignRepository campaignRepository;
-	private final PasswordEncoder passwordEncoder;
+    private static final String SESSION_KEY = "member";
+    private final MemberRepository memberRepository;
+    private final CampaignRepository campaignRepository;
+    private final PasswordEncoder passwordEncoder;
 
-	public MemberService(
-		MemberRepository memberRepository,
-		CampaignRepository campaignRepository,
-		PasswordEncoder passwordEncoder
-	) {
-		this.memberRepository = memberRepository;
-		this.campaignRepository = campaignRepository;
-		this.passwordEncoder = passwordEncoder;
-	}
+    public MemberService(
+        MemberRepository memberRepository,
+        CampaignRepository campaignRepository,
+        PasswordEncoder passwordEncoder
+    ) {
+        this.memberRepository = memberRepository;
+        this.campaignRepository = campaignRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-	public void signUpMember(SignUpRequest signUpRequest) {
-		if (memberRepository.findByEmail(signUpRequest.email()).isPresent()) {
-			throw new WiseShopException(WiseShopErrorCode.ALREADY_EXIST_MEMBER);
-		}
-		String encodePassword = passwordEncoder.encode(signUpRequest.password());
-		Member member = new Member(signUpRequest.email(), signUpRequest.name(), encodePassword);
-		memberRepository.save(member);
-	}
+    public void signUpMember(SignUpRequest signUpRequest) {
+        if (memberRepository.findByEmail(signUpRequest.email()).isPresent()) {
+            throw new WiseShopException(WiseShopErrorCode.ALREADY_EXIST_MEMBER);
+        }
+        String encodePassword = passwordEncoder.encode(signUpRequest.password());
+        Member member = new Member(signUpRequest.email(), signUpRequest.name(), encodePassword);
+        memberRepository.save(member);
+    }
 
-	public void signInMember(SignInRequest signInRequest, HttpSession session) {
-		Member member = memberRepository.findByEmail(signInRequest.email())
-			.orElseThrow(() -> new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND));
-		boolean matches = passwordEncoder.matches(signInRequest.password(), member.getPassword());
-		if (!matches) {
-			throw new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND);
-		}
-		session.setAttribute(SESSION_KEY, member);
-	}
+    public void signInMember(SignInRequest signInRequest, HttpSession session) {
+        Member member = memberRepository.findByEmail(signInRequest.email())
+            .orElseThrow(() -> new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND));
+        boolean matches = passwordEncoder.matches(signInRequest.password(), member.getPassword());
+        if (!matches) {
+            throw new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND);
+        }
+        session.setAttribute(SESSION_KEY, member);
+    }
 
-	public void deleteMember(Member member) {
-		List<Campaign> campaigns = campaignRepository.findCampaignByMemberId(member.getId());
-		if (campaigns.stream().anyMatch(it -> it.getState().equals(CampaignState.IN_PROGRESS))) {
-			throw new WiseShopException(WiseShopErrorCode.MEMBER_INPROGRESS_CAMPAIGN_EXIST);
-		}
-		memberRepository.deleteById(member.getId());
-	}
+    public void deleteMember(Member member) {
+        List<Campaign> campaigns = campaignRepository.findCampaignByMemberId(member.getId());
+        if (campaigns.stream().anyMatch(it -> it.getState().equals(CampaignState.IN_PROGRESS))) {
+            throw new WiseShopException(WiseShopErrorCode.MEMBER_INPROGRESS_CAMPAIGN_EXIST);
+        }
+        memberRepository.deleteById(member.getId());
+    }
 }

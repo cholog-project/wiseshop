d1f43147d5ae4966724592fff2ae10ffb1bd685a
Sumin <new.sumyang@gmail.com>
2025-02-09 11:59:51 +0900

    refactor: 캠페인 서비스 테스트 수정

--- src/test/java/cholog/wiseshop/domain/campaign/CampaignServiceTest.java
+++ src/test/java/cholog/wiseshop/domain/campaign/CampaignServiceTest.java
@@ -1,7 +1,8 @@
 package cholog.wiseshop.domain.campaign;
 
-import static cholog.wiseshop.domain.product.ProductRepositoryTest.getCreateProductRequest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.doNothing;
 
 import cholog.wiseshop.api.campaign.dto.request.CreateCampaignRequest;
@@ -17,10 +18,13 @@ import cholog.wiseshop.db.member.Member;
 import cholog.wiseshop.db.member.MemberRepository;
 import cholog.wiseshop.db.product.Product;
 import cholog.wiseshop.db.product.ProductRepository;
+import cholog.wiseshop.db.stock.Stock;
+import cholog.wiseshop.db.stock.StockRepository;
+import cholog.wiseshop.exception.WiseShopErrorCode;
+import cholog.wiseshop.exception.WiseShopException;
+import cholog.wiseshop.fixture.ProductFixture;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,8 +41,8 @@ public class CampaignServiceTest extends BaseTest {
     @Autowired
     private MemberRepository memberRepository;
 
-//    @Autowired
-//    private ProductService productService;
+    @Autowired
+    private StockRepository stockRepository;
 
     @Autowired
     private CampaignService campaignService;
@@ -49,6 +53,7 @@ public class CampaignServiceTest extends BaseTest {
     @AfterEach
     public void cleanUp() {
         productRepository.deleteAllInBatch();
+        stockRepository.deleteAllInBatch();
         campaignRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
     }
@@ -104,133 +109,106 @@ public class CampaignServiceTest extends BaseTest {
         assertThat(findCampaign.getGoalQuantity()).isEqualTo(10);
     }
 
-//
-//    @Test
-//    void 캠페인_조회하기() {
-//        // given
-//        CreateProductRequest request = getCreateProductRequest();
-//
-//        // when
-//        LocalDateTime startDate = LocalDateTime.of(2025, 1, 7, 10, 30);
-//        LocalDateTime endDate = LocalDateTime.of(2025, 1, 8, 10, 30);
-//        int goalQuantity = 5;
-//
-//        Long campaignId = campaignService.createCampaign(
-//            new CreateCampaignRequest(startDate, endDate, goalQuantity, request), null);
-//        ReadCampaignResponse response = campaignService.readCampaign(campaignId);
-//
-//        // then
-//        assertAll(
-//            () -> assertThat(response.campaignId()).isEqualTo(campaignId),
-//            () -> assertThat(response.product().name()).isEqualTo(request.name()),
-//            () -> assertThat(response.product().description()).isEqualTo(request.description()),
-//            () -> assertThat(response.product().price()).isEqualTo(request.price()),
-//            () -> assertThat(response.product().totalQuantity()).isEqualTo(request.totalQuantity())
-//        );
-//    }
-//
-//    @Test
-//    void 캠페인_조회하기_예외_잘못된_캠페인ID() {
-//        // given
-//        CreateProductRequest request = getCreateProductRequest();
-//
-//        // when
-//        LocalDateTime startDate = LocalDateTime.of(2025, 1, 7, 10, 30);
-//        LocalDateTime endDate = LocalDateTime.of(2025, 1, 8, 10, 30);
-//        int goalQuantity = 5;
-//
-//        Long campaignId = campaignService.createCampaign(
-//            new CreateCampaignRequest(startDate, endDate, goalQuantity, request), null);
-//        Long wrongId = campaignId + 1;
-//
-//        // then
-//        assertThatThrownBy(() -> campaignService.readCampaign(wrongId))
-//            .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @Test
-//    void 캠페인_시작_상태_변경_성공() {
-//        // given
-//        CreateProductRequest request = getCreateProductRequest();
-//
-//        // when
-//        LocalDateTime startDate = LocalDateTime.now().plus(50, ChronoUnit.MILLIS);
-//        LocalDateTime endDate = LocalDateTime.now().plusMinutes(10);
-//        int goalQuantity = 5;
-//
-//        Long campaignId = campaignService.createCampaign(
-//            new CreateCampaignRequest(startDate, endDate, goalQuantity, request), null);
-//
-//        // then
-//        Awaitility.await()
-//            .dontCatchUncaughtExceptions()
-//            .until(() -> {
-//                Campaign modifiedCampaign = campaignRepository.findById(campaignId)
-//                    .orElseThrow();
-//                return modifiedCampaign.getState().equals(CampaignState.IN_PROGRESS);
-//            });
-//    }
-//
-//    @Test
-//    void 캠페인_실패_상태_변경_성공() {
-//        // given
-//        CreateProductRequest request = getCreateProductRequest();
-//
-//        // when
-//        LocalDateTime startDate = LocalDateTime.now();
-//        LocalDateTime endDate = LocalDateTime.now().plus(100, ChronoUnit.MILLIS);
-//        int goalQuantity = 5;
-//
-//        Long campaignId = campaignService.createCampaign(
-//            new CreateCampaignRequest(startDate, endDate, goalQuantity, request), null);
-//        Campaign findCampaign = campaignRepository.findById(campaignId).orElseThrow();
-//
-//        // then
-//        Awaitility.await()
-//            .dontCatchUncaughtExceptions()
-//            .atMost(200, TimeUnit.MILLISECONDS)
-//            .untilAsserted(() -> {
-//                Campaign modifiedCampaign = campaignRepository.findById(findCampaign.getId())
-//                    .orElseThrow();
-//                assertThat(modifiedCampaign.getState()).isEqualTo(CampaignState.FAILED);
-//            });
-//    }
-//
-//    @Test
-//    void 캠페인이_시작됐는지_확인() {
-//        // given
-//        CreateProductRequest request = getCreateProductRequest();
-//        productService.createProduct(request);
-//
-//        // when
-//        LocalDateTime startDate = LocalDateTime.now().plusSeconds(1);
-//        LocalDateTime endDate = LocalDateTime.now().plusSeconds(10);
-//        int goalQuantity = 5;
-//
-//        Long campaignId = campaignService.createCampaign(
-//            new CreateCampaignRequest(startDate, endDate, goalQuantity, request), null);
-//        // then
-//        Awaitility.await()
-//            .atLeast(1, TimeUnit.SECONDS)
-//            .untilAsserted(() -> assertThat(campaignService.isStarted(campaignId)).isTrue());
-//    }
+
+    @Test
+    void 캠페인_조회하기() {
+        // given
+        CreateProductRequest request = ProductFixture.getCreateProductRequest();
+        Member member = new Member("123@123.123", "김수민", "비밀번호");
+        memberRepository.save(member);
+        Stock stock = stockRepository.save(new Stock(request.totalQuantity()));
+        LocalDateTime startDate = LocalDateTime.of(2025, 1, 7, 10, 30);
+        LocalDateTime endDate = LocalDateTime.of(2025, 1, 8, 10, 30);
+        Campaign campaign = campaignRepository.save(new Campaign(
+            startDate,
+            endDate,
+            2,
+            member
+        ));
+        productRepository.save(new Product(
+            request.name(),
+            request.description(),
+            request.price(),
+            campaign,
+            stock
+        ));
+
+        // when
+        ReadCampaignResponse response = campaignService.readCampaign(campaign.getId());
+
+        // then
+        assertAll(
+            () -> assertThat(response.campaignId()).isEqualTo(campaign.getId()),
+            () -> assertThat(response.product().name()).isEqualTo(request.name()),
+            () -> assertThat(response.product().description()).isEqualTo(request.description()),
+            () -> assertThat(response.product().price()).isEqualTo(request.price()),
+            () -> assertThat(response.product().totalQuantity()).isEqualTo(request.totalQuantity())
+        );
+    }
+
+    @Test
+    void 캠페인_조회하기_예외_잘못된_캠페인ID() {
+        // given
+        CreateProductRequest request = ProductFixture.getCreateProductRequest();
+        Member member = new Member("123@123.123", "김수민", "비밀번호");
+        memberRepository.save(member);
+        Stock stock = stockRepository.save(new Stock(request.totalQuantity()));
+        LocalDateTime startDate = LocalDateTime.of(2025, 1, 7, 10, 30);
+        LocalDateTime endDate = LocalDateTime.of(2025, 1, 8, 10, 30);
+        Campaign campaign = campaignRepository.save(new Campaign(
+            startDate,
+            endDate,
+            2,
+            member
+        ));
+        productRepository.save(new Product(
+            request.name(),
+            request.description(),
+            request.price(),
+            campaign,
+            stock
+        ));
+        Long wrongId = campaign.getId() + 1;
+
+        // then
+        assertThatThrownBy(() -> campaignService.readCampaign(wrongId))
+            .isInstanceOf(WiseShopException.class)
+            .hasMessage(WiseShopErrorCode.CAMPAIGN_NOT_FOUND.getMessage());
+    }
 
     @Test
     void 기간_내_캠페인_전체조회_확인() {
         // given
-        CreateProductRequest request = getCreateProductRequest();
-        LocalDateTime startDate = LocalDateTime.now().plusSeconds(1);
-        LocalDateTime endDate = LocalDateTime.now().plusSeconds(2);
-        int goalQuantity = 5;
-        campaignService.createCampaign(
-            new CreateCampaignRequest(startDate, endDate, goalQuantity, request), null);
+        setData();
 
         // when
         List<ReadCampaignResponse> result = campaignService.readAllCampaign();
 
         // then
-        Awaitility.await()
-            .atMost(101, TimeUnit.MILLISECONDS)
-            .untilAsserted(() -> assertThat(result).hasSize(1));
+        assertThat(result).hasSize(5);
+    }
+
+    void setData() {
+        CreateProductRequest request = ProductFixture.getCreateProductRequest();
+        Member member = new Member("123@123.123", "김수민", "비밀번호");
+        LocalDateTime startDate = LocalDateTime.of(2025, 1, 7, 10, 30);
+        LocalDateTime endDate = LocalDateTime.of(2025, 1, 8, 10, 30);
+        for (int i = 0; i < 5; i++) {
+            memberRepository.save(member);
+            Stock stock = stockRepository.save(new Stock(request.totalQuantity()));
+            Campaign campaign = campaignRepository.save(new Campaign(
+                startDate,
+                endDate,
+                2,
+                member
+            ));
+            productRepository.save(new Product(
+                request.name(),
+                request.description(),
+                request.price(),
+                campaign,
+                stock
+            ));
+        }
     }
 }

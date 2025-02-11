68e158fd92c29e819e94faeef485c3ef253428fe
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-02-11 14:13:30 +0900

    feat: 결제요청 시, 주문 데이터를 세션에 저장하는 기능 구현

--- src/main/java/cholog/wiseshop/api/order/controller/OrderController.java
+++ src/main/java/cholog/wiseshop/api/order/controller/OrderController.java
@@ -1,17 +1,19 @@
 package cholog.wiseshop.api.order.controller;
 
 import cholog.wiseshop.api.order.dto.request.CreateOrderRequest;
-import cholog.wiseshop.api.order.dto.request.ModifyOrderCountRequest;
+import cholog.wiseshop.api.order.dto.request.CreateOrderSessionRequest;
 import cholog.wiseshop.api.order.dto.response.OrderResponse;
 import cholog.wiseshop.api.order.service.OrderService;
 import cholog.wiseshop.common.auth.Auth;
 import cholog.wiseshop.db.member.Member;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,10 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
+    private final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @PostMapping("/orders/session")
+    public ResponseEntity<Void> createOrderSession(HttpSession session,
+                                                   @RequestBody CreateOrderSessionRequest request) {
+        session.setAttribute(request.paymentOrderId(),request.amount());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/orders")

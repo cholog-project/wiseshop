fc2cb82f722d2349ca9a09f0919264150fdd7e00
최준호 <junho5336@gmail.com>
2025-02-16 03:49:07 +0900

    refactor: api path 수정 (#88)
    * refactor: 주문 API 경로 수정
    
    * refactor: auth, 회원 경로 분리

--- src/main/java/cholog/wiseshop/api/member/service/MemberService.java
+++ src/main/java/cholog/wiseshop/api/member/service/MemberService.java
@@ -1,7 +1,5 @@
 package cholog.wiseshop.api.member.service;
 
-import cholog.wiseshop.api.member.dto.request.SignInRequest;
-import cholog.wiseshop.api.member.dto.request.SignUpRequest;
 import cholog.wiseshop.db.campaign.Campaign;
 import cholog.wiseshop.db.campaign.CampaignRepository;
 import cholog.wiseshop.db.campaign.CampaignState;
@@ -9,11 +7,7 @@ import cholog.wiseshop.db.member.Member;
 import cholog.wiseshop.db.member.MemberRepository;
 import cholog.wiseshop.exception.WiseShopErrorCode;
 import cholog.wiseshop.exception.WiseShopException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import java.util.List;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,47 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberService {
 
-    private static final String SESSION_KEY = "member";
     private final MemberRepository memberRepository;
     private final CampaignRepository campaignRepository;
-    private final PasswordEncoder passwordEncoder;
 
     public MemberService(
         MemberRepository memberRepository,
-        CampaignRepository campaignRepository,
-        PasswordEncoder passwordEncoder
+        CampaignRepository campaignRepository
     ) {
         this.memberRepository = memberRepository;
         this.campaignRepository = campaignRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public void signUpMember(SignUpRequest signUpRequest) {
-        if (memberRepository.findByEmail(signUpRequest.email()).isPresent()) {
-            throw new WiseShopException(WiseShopErrorCode.ALREADY_EXIST_MEMBER);
-        }
-        String encodePassword = passwordEncoder.encode(signUpRequest.password());
-        Member member = new Member(signUpRequest.email(), signUpRequest.name(), encodePassword);
-        memberRepository.save(member);
-    }
-
-    public void signInMember(SignInRequest signInRequest, HttpSession session) {
-        Member member = memberRepository.findByEmail(signInRequest.email())
-            .orElseThrow(() -> new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND));
-        boolean matches = passwordEncoder.matches(signInRequest.password(), member.getPassword());
-        if (!matches) {
-            throw new WiseShopException(WiseShopErrorCode.MEMBER_ID_NOT_FOUND);
-        }
-        session.setAttribute(SESSION_KEY, member);
-    }
-
-    public void signOut(HttpSession session, HttpServletResponse response) {
-        session.invalidate();
-
-        Cookie sessionCookie = new Cookie("JSESSIONID", null);
-        sessionCookie.setMaxAge(0);     // 쿠키 즉시 만료
-        sessionCookie.setPath("/");     // 쿠키의 경로 설정
-        response.addCookie(sessionCookie);
     }
 
     public void deleteMember(Member member) {

ae4f79d78d79992c708c64c9b32d4b6916d385b9
최준호 <junho5336@gmail.com>
2025-02-16 00:31:30 +0900

    feat: 로그인 체크용 메소드 구현 (#79)

--- src/main/java/cholog/wiseshop/api/member/controller/MemberController.java
+++ src/main/java/cholog/wiseshop/api/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,13 @@ public class MemberController {
         HttpServletRequest httpServletRequest) {
         HttpSession session = httpServletRequest.getSession();
         memberService.signInMember(signInRequest, session);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Void> checkSession(
+        @Auth Member member
+    ) {
         return ResponseEntity.ok().build();
     }
 

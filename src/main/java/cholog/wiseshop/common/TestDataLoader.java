881b9b5ddde1766829b6044c3ec38a12aafe728e
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-02-15 17:00:37 +0900

    feat: local 프로필로 실행 시, 테스트 데이터 추가
    - 판매자와 구매자 회원 데이터
    - 판매자가 등록한 캠페인 1건

---
+++ src/main/java/cholog/wiseshop/common/TestDataLoader.java
@@ -0,0 +1,71 @@
+package cholog.wiseshop.common;
+
+import cholog.wiseshop.db.campaign.Campaign;
+import cholog.wiseshop.db.campaign.CampaignRepository;
+import cholog.wiseshop.db.campaign.CampaignState;
+import cholog.wiseshop.db.member.Member;
+import cholog.wiseshop.db.member.MemberRepository;
+import cholog.wiseshop.db.product.Product;
+import cholog.wiseshop.db.product.ProductRepository;
+import cholog.wiseshop.db.stock.Stock;
+import cholog.wiseshop.db.stock.StockRepository;
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Profile("local")
+@Component
+public class TestDataLoader implements CommandLineRunner {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CampaignRepository campaignRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    StockRepository stockRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Member supplier = memberRepository.save(new Member(
+            "supplier@test.com",
+            "판매자",
+            passwordEncoder.encode("12341234")
+        ));
+        memberRepository.save(new Member(
+            "customer@test.com",
+            "구매자",
+            passwordEncoder.encode("12341234")
+        ));
+        LocalDateTime now = LocalDateTime.now();
+        Campaign campaign = campaignRepository.save(Campaign.builder()
+            .startDate(now.plusDays(1))
+            .endDate(now.plusDays(2))
+            .goalQuantity(100)
+            .soldQuantity(0)
+            .state(CampaignState.IN_PROGRESS)
+            .member(supplier)
+            .now(now)
+            .build()
+        );
+        Stock stock = stockRepository.save(new Stock(500));
+        productRepository.save(Product.builder()
+            .name("에그타르트")
+            .description("에그타르트 맛있어요")
+            .price(5000)
+            .stock(stock)
+            .campaign(campaign)
+            .build()
+        );
+    }
+}
